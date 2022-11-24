d8f564df6003e61227a6f66063b5d1947344188d
Jesse Giles <jesse.giles@telus.com>
2022-11-24 16:49:55 -0700

    created index.js, recoded request into callback function, created test file and verified results

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,28 @@
-const arrs = process.argv.slice(2);
+
 const request = require('request');
 
-const requestedCat = arrs.toString();
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${requestedCat}`, (error, response, body) => {
-  if (error) {
-    return console.log('statusCode:', response && response.statusCode, `\nerror encountered:`, error);
+const fetchBreedDescription = function(breedName, callback) {
+  
+  if (breedName === "") {
+    return callback('Please enter the type of cat to search.', null);
   }
 
-  if (requestedCat === "") {
-    return console.log('Please enter the type of cat to search.');
-  }
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      return callback(`error encountered: ${error}`, null);
+    }
 
-  const data = JSON.parse(body); //convert html string of body received from request into a JSON object
+    const data = JSON.parse(body); //convert html string of body received from request into a JSON object
 
-  if (data.length === 0) {
-    return console.log('Cat not found in database.');
-  } else {
-    console.log(data[0].description); //log a value of a set key in JSON obj
-  }
+    if (data.length === 0) {
+      return callback('Cat not found in database.', null);
+    } else {
+      callback(null, data[0].description); //log a value of a set key in JSON obj
+    }
 
-});
+  });
+
+};
+
+module.exports = { fetchBreedDescription };
